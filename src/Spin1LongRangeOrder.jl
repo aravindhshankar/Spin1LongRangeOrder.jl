4265661dfc0f215e68e28f771532126cb25c6ddc
aravindhshankar <aravindhswaminathan@gmail.com>
2026-04-26 23:46:41 +0200

    NightSync

--- src/Spin1LongRangeOrder.jl
+++ src/Spin1LongRangeOrder.jl
@@ -2,14 +2,15 @@ module Spin1LongRangeOrder
 
 using ITensors, ITensorMPS
 using Plots
+using HDF5
 gr()
 
 let
   N = 100
-  sites = siteinds("S=1", N)
+  sites = siteinds("S=1", N; conserve_sz=false)
   h = 0.0
   J = -1.0
-  g = 0.5
+  g = 1.5
 
   os = OpSum()
   for j = 1:N-1
@@ -47,10 +48,14 @@ let
   noise_p = [1e-6]
 
   psi0 = random_mps(sites; linkdims=2)
-
+  # instate = [isodd(n) ? "Up" : "Up" for n in 1:N]
+  # psi0 = random_mps(sites, instate; linkdims=10)
+  # psi_fm = random_mps(sites, instate; linkdims=10)
   energy, psi_p = dmrg(H_prelim, psi0; nsweeps=nsweeps_p, maxdim=maxdim_p,
     cutoff=cutoff_p, mindim=mindim_p,
     eigsolve_krylovdim=eigsolve_krylovdim_p, noise=noise_p)
+
+  psi_fm = psi0
 
   nsweeps = 2
   maxdim = [100]
@@ -61,19 +66,32 @@ let
   energy, psi_fm = dmrg(H_fm, psi_p; nsweeps, maxdim, cutoff, mindim=mindim,
     eigsolve_krylovdim=eigsolve_krylovdim)
 
-  nsweeps = 5
-  maxdim = [20, 40, 60, 80, 100]
-  mindim = [1, 1]
+  nsweeps = 10
+  maxdim = [20, 40, 60, 80, 100, 150, 200]
+  mindim = [10, 10]
   eigsolve_krylovdim = 3
   cutoff = [1E-12]
   noise = [0]
   energy, psi = dmrg(H_pert, psi_fm; nsweeps, maxdim, cutoff, mindim=mindim,
     eigsolve_krylovdim=eigsolve_krylovdim)
+
+  # writeToFile("mps_test")
+  f = h5open("sampleMPS.h5", "w") do f
+    write(f, "psi", psi)
+  end
+
   println("Energy = ", energy)
+  H2 = inner(H_pert, psi, H_pert, psi)
+  E = inner(psi', H_pert, psi)
+  var = H2 - E^2
+  @show var
   zzcorr = correlation_matrix(psi, "Sz", "Sz")
   xxcorr = correlation_matrix(psi, "Sx", "Sx")
 
-  p = plot(abs.(zzcorr[Int64(N // 2), :]), ms=5, lw=2)
+  xplotvals = range(start=1, length=100, step=1)
+  line1 = abs.(zzcorr[Int64(N // 2), :])
+  line2 = abs.(xxcorr[Int64(N // 2), :])
+  p = plot(xplotvals, [line1, line2], ms=5, lw=2)
   plot!(xscale=:identity, yscale=:log10, minorgrid=true)
   # plot!(xscale=:identity, yscale=:identity, minorgrid=true)
   # plot!(xscale=:log10, yscale=:log10, minorgrid=true)
