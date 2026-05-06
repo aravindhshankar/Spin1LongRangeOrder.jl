25102c7c609d2e2111099897d3af2c8b899708d1
aravindhshankar <aravindhswaminathan@gmail.com>
2026-05-06 18:52:43 +0200

    scan for critical point with 100 spins on argo

--- example/eval.jl
+++ example/eval.jl
@@ -4,68 +4,53 @@ using ITensors, ITensorMPS
 using Spin1LongRangeOrder.Hamiltonians
 using HDF5
 using Plots
+using LinearAlgebra
 gr()
 ##
 let
-  # println(BLAS.get_config())  
-  N = 200
+  println(BLAS.get_config())
+  N = 100
   sites = siteinds("S=1", N; conserve_sz=false)
-  h = 0.0
   J = -1.0
-  g = 1.5 #g>0 orders in X-Y plane, g=0 is the critical point, g<0 orders in Z direction
+  g = 0.5 #g>0 orders in X-Y plane, g=0 is the critical point, g<0 orders in Z direction
+  g2 = -0.35 #g2 >0 favors X-Y plans, g2<0 favors Z axis
+  #we saw g2 = -0.3 is in XY FM state
+  #       g2 = -0.4 is in Z FM state (gapped)
 
-#   H_fm = build_fm_hamiltonian(sites, J)
-#   H_prelim = build_prelim_hamiltonian(sites, J)
-  H_pert = build_pert_hamiltonian(sites, J, g)
+  #   H_fm = build_fm_hamiltonian(sites, J)
+  #   H_prelim = build_prelim_hamiltonian(sites, J)
+  # H_pert = build_pert_hamiltonian(sites, J, g)
+  H_pert = build_g1g2_hamiltonian(sites, J, g, g2)
 
-#   nsweeps_p = 3
-#   maxdim_p = [5, 20, 50] # gradually increase states kept
-#   # mindim = [1, 1, 5, 10, 20, 50]
-#   mindim_p = [1]
-#   eigsolve_krylovdim_p = 3 #default 3
-#   cutoff_p = [1E-12] # desired truncation error
-#   noise_p = [1e-6]
-
-#   psi0 = random_mps(sites; linkdims=2)
-    instate = ["X+" for n in 1:N]
-    # instate = ["Up" for n in 1:N]
-    psi_0 = random_mps(sites, instate; linkdims=10)
-    # psi_fm = random_mps(sites, instate; linkdims=10)
-
-#   energy, psi_p = dmrg(H_prelim, psi_0; nsweeps=nsweeps_p, maxdim=maxdim_p,
-#     cutoff=cutoff_p, mindim=mindim_p,
-#     eigsolve_krylovdim=eigsolve_krylovdim_p, noise=noise_p)
-
-#   nsweeps = 2
-#   maxdim = [100]
-#   mindim = [1, 1]
-#   eigsolve_krylovdim = 3
-#   cutoff = [1E-12]
-#   noise = [0]
-#   energy, psi_fm = dmrg(H_fm, psi_p; nsweeps, maxdim, cutoff, mindim=mindim,
-#     eigsolve_krylovdim=eigsolve_krylovdim)
+  #   psi0 = random_mps(sites; linkdims=2)
+  instate = ["X+" for n in 1:N]
+  # instate = ["Up" for n in 1:N]
+  psi_0 = random_mps(sites, instate; linkdims=10)
+  # psi_fm = random_mps(sites, instate; linkdims=10)
 
 
   ### Load the recently stopped state and continue with the perturbed Hamiltonian
-#   psi_load = load_simulation("sampleMPS.h5")
-#   psi_load = replace_siteinds(psi_load, sites)  # Replace siteinds to match the new sites
+  #   psi_load = load_simulation("sampleMPS.h5")
+  #   psi_load = replace_siteinds(psi_load, sites)  # Replace siteinds to match the new sites
 
-  
-  nsweeps = 50
-#   maxdim = [100, 150, 200, 200 ,200 ,200,200, 200, 200, 200, 220, 220, 220, 250, 250, 250, 250, 250, 300, 300, 300, 350, 350, 350, 400, 400] # gradually increase states kept
-  maxdim = [100, 200, 220, 250, 300, 350]
-  mindim = [10, 10]
+
+  nsweeps = 20
+  #   maxdim = [100, 150, 200, 200 ,200 ,200,200, 200, 200, 200, 220, 220, 220, 250, 250, 250, 250, 250, 300, 300, 300, 350, 350, 350, 400, 400] # gradually increase states kept
+  maxdim = [50, 100, 200, 220]
+  mindim = [10, 10, 2]
   eigsolve_krylovdim = 5
   cutoff = [1E-12]
-  noise = [0]
-# @profview dmrg(H_pert, psi_fm; nsweeps, maxdim, cutoff, mindim=mindim,
-#     eigsolve_krylovdim=eigsolve_krylovdim)
-  energy, psi = dmrg(H_pert, psi_0; nsweeps, maxdim, cutoff, mindim=mindim,
-    eigsolve_krylovdim=eigsolve_krylovdim)
+  noise = [0, 1E-6, 0]
+  obsparams = (energy_tol=1E-3, minsweeps=5, energy_type=Float64)
+  observer = DMRGObserver(; obsparams...)
+  # @profview dmrg(H_pert, psi_fm; nsweeps, maxdim, cutoff, mindim=mindim,
+  #     eigsolve_krylovdim=eigsolve_krylovdim)
+  energy, psi = dmrg(H_pert, psi_0; nsweeps, maxdim, cutoff, mindim,
+    eigsolve_krylovdim, noise, observer)
 
   # writeToFile("mps_test")
   save_simulation("sampleMPS.h5", psi)
-  
+
 
   println("Energy = ", energy)
   H2 = inner(H_pert, psi, H_pert, psi)
@@ -75,18 +60,20 @@ let
   zzcorr = correlation_matrix(psi, "Sz", "Sz")
   xxcorr = correlation_matrix(psi, "Sx", "Sx")
   yycorr = correlation_matrix(psi, "Sy", "Sy")
+  pmcorr = correlation_matrix(psi, "S+", "S-")
 
   xplotvals = range(start=1, length=N, step=1)
   line1 = abs.(zzcorr[Int64(N // 2), :])
   line2 = abs.(xxcorr[Int64(N // 2), :])
   line3 = abs.(yycorr[Int64(N // 2), :])
-  p = plot(xplotvals, [line1, line2, line3], ms=5, lw=2, label=["Sz-Sz" "Sx-Sx" "Sy-Sy"], xlabel="Distance", ylabel="Correlation",
-     title="g = $(g)", legend=:topright)
+  line4 = abs.(pmcorr[Int64(N // 2), :])
+  p = plot(xplotvals, [line1, line2, line3, line4], ms=5, lw=2,
+    label=["Sz-Sz" "Sx-Sx" "Sy-Sy" "+-"], xlabel="Distance", ylabel="Correlation",
+    title="g1 = $(g) g2 = $(g2)", legend=:topright)
   plot!(xscale=:identity, yscale=:log10, minorgrid=true)
   # plot!(xscale=:identity, yscale=:identity, minorgrid=true)
   # plot!(xscale=:log10, yscale=:log10, minorgrid=true)
   savefig(p, "figure.png")
   println("saved")
-  gui(p)
-  return 
+  return
 end
--- scripts/scang2.jl
+++ scripts/scang2.jl
@@ -1,45 +1,82 @@
+using MKL
 using Spin1LongRangeOrder
 using Spin1LongRangeOrder.Hamiltonians
 using ITensorMPS
 using Printf
+using Plots
+
+gr()
 
 
 function runDMRGaction(hamparams)
-  N = 10
+  N = 100
   # hamparams = (J=1, g1=0.5, g2=-0.5)
   sites = siteinds("S=1", N)
   Ham = build_g1g2_hamiltonian(sites; hamparams...)
   println("Built hamiltonian")
+  @show hamparams
 
   # psi_0 = randomMPS(sites, linkdims=10)
   # instate = ["Up" for n in 1:N]
   instate = ["X+" for _ in 1:N]
   psi_0 = random_mps(sites, instate; linkdims=10)
 
-  nsweeps = 6
-  maxdim = [10, 20]
-  mindim = [1, 1]
-  eigsolve_krylovdim = 3
+  nsweeps = 20
+  maxdim = [20, 50, 100, 100, 150, 200, 200, 220, 250, 300]
+  mindim = [10, 10, 10, 1]
+  eigsolve_krylovdim = 5
   cutoff = [1E-12]
   noise = [1E-6, 0]
-  obsparams = (energy_tol=1E-5, minsweeps=5, energy_type=Float64)
+  obsparams = (energy_tol=1E-4, minsweeps=5, energy_type=Float64)
   observer = DMRGObserver(; obsparams...)
 
   energy, psi = dmrg(Ham, psi_0; nsweeps, maxdim, cutoff, mindim=mindim,
     eigsolve_krylovdim=eigsolve_krylovdim, observer, noise)
 
-  fmt(x) = @sprintf("%.2f", x)
-  savepath = "data/N10/"
+  fmt(x) = @sprintf("%.3f", x)
+  savepath = "data/N$(N)/"
   mkpath(savepath)
-  parts = [String(k) * fmt(v) for (k, v) in pairs(hamparams)]
+  parts = [String(k) * "_" * fmt(v) for (k, v) in pairs(hamparams)]
   filename = savepath * join(parts, "") * ".h5"
   save_simulation(filename, psi, Dict(pairs(hamparams)))
-  println("saved")
+  println("saved to filename : " * filename)
+
+  #-----------------------------------------savefig--------------
+  figsavepath = "pngfigs/N$(N)/"
+  mkpath(figsavepath)
+  figfilename = figsavepath * join(parts, "") * ".png"
+  H2 = inner(Ham, psi, Ham, psi)
+  E = inner(psi', Ham, psi)
+  var = H2 - E^2
+  @show var
+  zzcorr = correlation_matrix(psi, "Sz", "Sz")
+  xxcorr = correlation_matrix(psi, "Sx", "Sx")
+  yycorr = correlation_matrix(psi, "Sy", "Sy")
+  pmcorr = correlation_matrix(psi, "S+", "S-")
+
+  xplotvals = range(start=1, length=N, step=1)
+  line1 = abs.(zzcorr[Int64(N // 2), :])
+  line2 = abs.(xxcorr[Int64(N // 2), :])
+  line3 = abs.(yycorr[Int64(N // 2), :])
+  line4 = abs.(pmcorr[Int64(N // 2), :])
+  p = plot(xplotvals, [line1, line2, line3, line4], ms=5, lw=2,
+    label=["Sz-Sz" "Sx-Sx" "Sy-Sy" "+-"], xlabel="Distance", ylabel="Correlation",
+    title="g1 = $(hamparams.g1) g2 = $(hamparams.g2)", legend=:topright)
+  plot!(xscale=:identity, yscale=:log10, minorgrid=true)
+  savefig(p, figfilename)
+  println("saved figure to file: " * figfilename)
+
+  return
 end
 
 let
-  gval = 0.6
-  hamparams = (J=1, g1=gval, g2=-0.5)
+  gval = 0.5
+  g2list = [(J=-1, g1=gval, g2=g2val)
+            for g2val in range(start=-0.4, step=0.01, length=10)]
+  #stops at -0.31
+  # idx = 1
+  idx = Base.parse(Int, ENV["SLURM_ARRAY_TASK_ID"])
+  hamparams = g2list[idx]
   runDMRGaction(hamparams)
 end
 
