76c416682a47c4e803b4a5e110c3a95e9ee6bfac
aravindhshankar <aravindhswaminathan@gmail.com>
2026-06-01 11:05:57 +0200

    dmrg is agnostic to choice of basis lol

--- src/SpinHalfIsing.jl
+++ src/SpinHalfIsing.jl
@@ -3,23 +3,25 @@ using Plots
 gr()
 
 let
-  N = 100
+  N = 20
   sites = siteinds("S=1/2", N)
-  h = 1.0
-  J = -1.0
+  h = 1.5
+  J = 1.0 # - for FM, + for AFM
 
+  jop = "Sz"
+  hop = "Sx"
   os = OpSum()
   for j = 1:N-1
-    os += 4 * J, "Sz", j, "Sz", j + 1
-    os += 2 * h, "Sx", j
+    os += 4 * J, jop, j, jop, j + 1
+    os += 2 * h, hop, j
   end
-  os += 2 * h, "Sx", N
+  os += 2 * h, hop, N
   # os += J,"Sz",N,"Sz",1 #PBC
   H = MPO(os, sites)
 
-  nsweeps = 7 # number of sweeps is 5
+  nsweeps = 10 # number of sweeps is 5
   maxdim = [5, 10, 20, 50, 100, 200] # gradually increase states kept
-  mindim = [1, 1, 5, 10, 20, 50, 100]
+  mindim = [2, ]
   eigsolve_krylovdim = 10 #default 3
   cutoff = [1E-12] # desired truncation error
 
@@ -28,7 +30,17 @@ let
   energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff, mindim=mindim,
     eigsolve_krylovdim=eigsolve_krylovdim)
   println("Energy = ", energy)
+
+  H2 = inner(H, psi, H, psi)   # Computes <H^2>
+  E = inner(psi', H, psi)      # Computes <H>
+  var = H2 - E^2               # Computes Variance
+  @show var 
+
   zzcorr = correlation_matrix(psi, "Sz", "Sz")
+  magz = sum(expect(psi, "Sz")) / N 
+  magx = sum(expect(psi, "Sx")) / N 
+  @show magz 
+  @show magx 
   maxy = maximum(zzcorr)
   miny = minimum(zzcorr)
   # @show(zzcorr[1,:])
