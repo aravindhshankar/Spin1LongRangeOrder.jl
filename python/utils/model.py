2fc97a55f61b233cf03bd4bc7d58e123736b02a7
Shankar Aravindh Swaminathan <ashankar@ictp.it>
2026-06-03 17:52:52 +0200

    began refactor of tenpy_idmrg=

---
+++ python/utils/model.py
@@ -0,0 +1,59 @@
+from tenpy.models.model import CouplingMPOModel
+from tenpy.networks.site import SpinSite
+from dataclasses import dataclass
+
+
+class Spin1Chain(CouplingMPOModel):
+    """
+    Spin-1 chain with Hamiltonian
+
+        H = -J Σ_i S_i · S_{i+1}
+            + g1 Σ_i (S_i^z)^2
+            + g2 Σ_i (S_i^z)^2 (S_{i+1}^z)^2
+
+    Infinite boundary conditions (iMPS / iDMRG).
+    """
+
+    def init_sites(self, model_params):
+        conserve = model_params.get("conserve", None)
+        site = SpinSite(S=model_params["S"], conserve=conserve)
+        return site
+
+    def init_terms(self, model_params):
+        J = model_params.get("J", 1.0)
+        g1 = model_params.get("g1", 0.0)
+        g2 = model_params.get("g2", 0.0)
+
+        # --- nearest-neighbor Heisenberg coupling ---
+        # H = -J (SxSx + SySy + SzSz)
+
+        for u1, u2, dx in self.lat.pairs["nearest_neighbors"]:
+            self.add_coupling(0.5*J, u1, "Sp", u2, "Sm", dx, plus_hc=True)
+            self.add_coupling(J, u1, "Sz", u2, "Sz", dx)
+
+        # --- single-ion anisotropy g1 (Sz)^2 ---
+        for u in range(len(self.lat.unit_cell)):
+            self.add_onsite(g1, u, "Sz Sz")
+
+        # --- biquadratic anisotropy g2 (Sz_i)^2 (Sz_j)^2 ---
+        for u1, u2, dx in self.lat.pairs["nearest_neighbors"]:
+            self.add_coupling(g2, u1, "Sz Sz", u2, "Sz Sz", dx)
+        # for u1, u2, dx in self.lat.pairs["nearest_neighbors"]:
+        #     self.add_multi_coupling(
+        #         g2,
+        #         [
+        #             (u1, "Sz Sz", 0),
+        #             (u2, "Sz Sz", dx),
+        #         ],
+        #     )
+
+
+@dataclass
+class ModelParams:
+    L: int = 2
+    S: int = 1
+    J: float = -1.0
+    g1: float = 0.2
+    g2: float = 0.0
+    bc_MPS: str = "infinite"
+    conserve: None = None
