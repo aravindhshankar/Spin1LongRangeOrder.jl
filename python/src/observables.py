efb8b6f5fe147fe2224eff85c2b1c61531a79e2a
Shankar Aravindh Swaminathan <ashankar@ictp.it>
2026-06-24 19:48:18 +0200

    added plotting of all ops in idmrg

--- python/src/observables.py
+++ python/src/observables.py
@@ -10,6 +10,13 @@ def SzSz_corr(psi, length=10, connected=False):
     
     return corrfn
 
+def SpSm_corr(psi, length=10, connected=False):
+    corrfn = psi.correlation_function("Sp", "Sm", [0], length)[0]
+    if connected: 
+        print("CONNECTED CORRELATION NOT IMPLEMENTED FOR SpSm_corr")
+    
+    return corrfn
+
 def charge_corr(psi, length=10, connected=False):
     charge_corr_fn = psi.correlation_function("Ntot", "Ntot", [0], length)[0]
     if connected : 
@@ -32,3 +39,29 @@ def electron_corr(psi, length=10, whichspin="tot"):
             raise Exception("Unavailble electron type : allowed options d | u | tot")
     return electron_corr_fn
 
+
+
+import numpy as np
+
+def denoise_rolling(y, window=10):
+    """
+    Smooth a 1D signal using a centered rolling average.
+    Supports both even and odd window sizes.
+    
+    Returns a list of same length as input.
+    """
+    y = np.asarray(y, dtype=float)
+
+    if window < 1:
+        raise ValueError("window must be >= 1")
+
+    # Symmetric padding (keeps length stable)
+    pad_left = window // 2
+    pad_right = window - 1 - pad_left
+
+    y_padded = np.pad(y, (pad_left, pad_right), mode='edge')
+
+    kernel = np.ones(window) / window
+    y_smooth = np.convolve(y_padded, kernel, mode='valid')
+
+    return y_smooth.tolist()
