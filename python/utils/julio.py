e3aa1045d91686960bb2c661e149adfb49435de3
aravindhshankar <aravindhswaminathan@gmail.com>
2026-08-11 12:26:46 +0200

    bugfixes in julio.py

--- python/utils/julio.py
+++ python/utils/julio.py
@@ -8,8 +8,17 @@ import os
 def load_correlations(path):
 
     with h5py.File(path, "r") as f:
+        if "params" in f: 
+            params = {} 
+            for key in f["params"].keys(): 
+                value = f["params"][key][()]
+                if isinstance(value, bytes): 
+                    value = value.decode("utf-8") 
+                params[key] = value 
+        else: 
+            params = None
         tdict = {
-            "szmat":       f["szmat"][()],
+            "szmat": f["szmat"][()],
             "chargemat":   f["chargemat"][()],
             "spmmat":      f["spmmat"][()],
             "elecupmat":   f["elecupmat"][()],
@@ -18,14 +27,19 @@ def load_correlations(path):
             "ntot_expect": f["ntot_expect"][()],
             "xspin2":      f["xspin2"][()] if "xspin2" in f else None,
             "spin2vec":    f["spin2vec"][()] if "spin2vec" in f else None,
+            "params" : params
         }
         N = tdict["szmat"].shape[0]
-        print(type(tdict["szmat"]), type(tdict["sz_expect"]))
-        tdict["szconn"] = tdict["szmat"][N//2, N//2 : ] 
-        - tdict["sz_expect"][N//2] * tdict["sz_expect"][N//2: ]
-        tdict["chargeconn"] = tdict["chargemat"][N//2, N//2 : ]
-        - tdict["ntot_expect"][N//2] * tdict["ntot_expect"][N//2: ]
-        
+        dV = params["dV"]
+        Vpm = params["Vpp"] + dV
+        tdict["N"] = N
+        tdict["dV"] = dV
+        tdict["Vpm"] = Vpm
+        tdict["szconn"] = tdict["szmat"][N//2, N//2 : ] - tdict["sz_expect"][N//2] * tdict["sz_expect"][N//2: ]
+        tdict["chargeconn"] = tdict["chargemat"][N//2, N//2 : ] - tdict["ntot_expect"][N//2] * tdict["ntot_expect"][N//2: ]
+        tdict["spmconn"] = tdict["spmmat"][N//2, N//2 : ]
+        tdict["electron_conn"] = 1.0 * (tdict["elecupmat"][N//2, N//2 : ] + tdict["elecdnmat"][N//2, N//2 : ])
+
     
     return tdict
 
@@ -41,7 +55,9 @@ def load_correlations_files(paths):
 
     return pd.DataFrame(rows)
 
-
+def select_N_dv_from_df(N, dV, df):
+    """Select rows from a DataFrame based on N and dV values."""
+    return df[(df["N"] == N) & (np.isclose(df["dV"], dV))]
 
 def corr_filename_builder(
     N, t, U, Vpp, dV, prefix="data/Hubbard/", DATAROOT="../../"
